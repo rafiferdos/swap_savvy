3af8388e688b89abdb9e07b1fda29ce736ac0a56
Rafi Ferdos <rafiferdos@gmail.com>
2024-05-10 16:55:40 +0600

    chore Update Home page carousel margin-top for better alignment

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,7 @@ import Carousel from "./Carousel";
 const Home = () => {
     return (
         <div className="space-y-10 md:space-y-16 lg:space-y-20 w-11/12 max-w-7xl container mx-auto">
-            <div className="flex items-center justify-center mt-8 md:mt-16 lg:mt-24">
+            <div className="flex items-center justify-center mt-8 md:mt-16 lg:mt-20">
                 <Carousel />
             </div>
             <div className="flex items-center justify-center">

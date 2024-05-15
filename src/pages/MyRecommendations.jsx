f6e1aeefb54429f52e5ac703f56fa8d55058ef64
Rafi Ferdos <rafiferdos@gmail.com>
2024-05-15 16:42:02 +0600

    refactor: Update MyRecommendations page to fetch and display user-specific recommendations

--- src/pages/MyRecommendations.jsx
+++ src/pages/MyRecommendations.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from "../provider/AuthProvider";
 
 const MyRecommendations = () => {
 
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
     const [recommendations, setRecommendations] = useState([]);
 
@@ -16,7 +16,10 @@ const MyRecommendations = () => {
                 setRecommendations(response.data)
                 console.log(recommendations)
             })
-    }, []);
+    }, [userEmail]);
+
+    const { product_name, product_brand, product_image_url, query_title, boycott_reason, user_display_name, user_email, user_img_url, datePosted, recommendation_count } = recommendations 
+
 
     return (
         <div className="container mx-auto max-w-7xl w-11/12">
@@ -30,9 +33,9 @@ const MyRecommendations = () => {
                                 <thead>
                                     <tr>
                                         <th>Name</th>
-                                        <th>Job</th>
+                                        <th>Rec. Description</th>
                                         <th>Favorite Color</th>
-                                        <th></th>
+                                        <th>Delete</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -44,11 +47,11 @@ const MyRecommendations = () => {
                                                         <div className="flex items-center gap-3">
                                                             <div className="avatar">
                                                                 <div className="mask mask-squircle w-12 h-12">
-                                                                    <img src="https://img.daisyui.com/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+                                                                    <img src={user_img_url} alt="Avatar Tailwind CSS Component" />
                                                                 </div>
                                                             </div>
                                                             <div>
-                                                                <div className="font-bold">Hart Hagerty</div>
+                                                                <div className="font-bold">{user_display_name}</div>
                                                                 <div className="text-sm opacity-50">United States</div>
                                                             </div>
                                                         </div>
@@ -60,24 +63,15 @@ const MyRecommendations = () => {
                                                     </td>
                                                     <td>Purple</td>
                                                     <th>
-                                                        <button className="btn btn-ghost btn-xs">details</button>
+                                                        <button className="btn btn-circle btn-outline">
+                                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                                                        </button>
                                                     </th>
                                                 </tr>
                                             )
                                         })
                                     }
                                 </tbody>
-                                {/* foot */}
-                                <tfoot>
-                                    <tr>
-                                        <th></th>
-                                        <th>Name</th>
-                                        <th>Job</th>
-                                        <th>Favorite Color</th>
-                                        <th></th>
-                                    </tr>
-                                </tfoot>
-
                             </table>
                         </div>
                     </div>
